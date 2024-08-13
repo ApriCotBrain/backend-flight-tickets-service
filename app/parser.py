d3f479cc3fadaee8b23d165421672c6fb2f30575
Ольга Мелихова <melihova.o@yandex.ru>
2024-08-13 15:19:29 +0300

    feat: parser tests

--- app/parser.py
+++ app/parser.py
@@ -1,180 +1,126 @@
-import tempfile
 from datetime import datetime
 from decimal import Decimal
-from pathlib import Path
+import xml.etree.ElementTree as ET
 
-import aiofiles
-import xmltodict
-from fastapi import UploadFile
+from app.schemas import Flight, Price, Ticket
 
 
-async def get_temp_data_path(xml_file: UploadFile) -> str:
-    """Save file in temp folder and return its path."""
-    temp_dir = Path(tempfile.gettempdir())
-    xml_data_path = temp_dir / xml_file.filename
-    contents = xml_file.file.read()
-    async with aiofiles.open(xml_data_path, "wb") as file:
-        await file.write(contents)
-    return str(xml_data_path)
+async def parse_all_flights(xml_data: str) -> list[ET.Element]:
+    """
+    Parse an XML string containing flight data and extract all priced itineraries.
+
+    Returns a list of ElementTree elements representing the priced itineraries.
+    """
+    tree = ET.parse(xml_data)
+    root = tree.getroot()
+
+    target_tags = root.findall("PricedItineraries/")
+
+    return target_tags
 
 
-async def get_all_flights(xml_data_path: str) -> list:
-    """Read the xml file and convert it to a dictionary."""
-    async with aiofiles.open(xml_data_path, "r") as file:
-        xml_data = await file.read()
-        xml_dict = xmltodict.parse(xml_data)["AirFareSearchResponse"][
-            "PricedItineraries"
-        ]
+async def parse_pricing(target_tags: list[ET.Element]) -> list[Price]:
+    """
+    Extract pricing information from a list of ElementTree elements.
 
-        for flight in xml_dict["Flights"]:
-            flight["Pricing"]["currency"] = flight["Pricing"].pop("@currency")
+    Returns a list of Price objects containing the extracted pricing data.
+    """
+    prices_values = []
 
-            price_values = flight["Pricing"]["ServiceCharges"]
-            for item in price_values:
-                item["type"] = item.pop("@type")
-                item["charge_type"] = item.pop("@ChargeType")
-                item["text"] = item.pop("#text")
+    for child in target_tags:
+        flight_prices = child.find("Pricing")
+        currency = flight_prices.get("currency")
+        service_charges = flight_prices.findall("ServiceCharges")
 
-            flight_values = flight["OnwardPricedItinerary"]["Flights"]["Flight"]
-            if isinstance(flight_values, list):
-                for item in flight_values:
-                    item["Carrier"]["id"] = item["Carrier"].pop("@id", item["Carrier"])
-                    item["Carrier"]["text"] = item["Carrier"].pop(
-                        "#text", item["Carrier"]
-                    )
-            if isinstance(flight_values, dict):
-                flight_values["Carrier"]["id"] = flight_values["Carrier"].pop("@id")
-                flight_values["Carrier"]["text"] = flight_values["Carrier"].pop("#text")
-
-        return xml_dict
+        for prices in service_charges:
+            type = prices.get("type")
+            charge_type = prices.get("ChargeType")
+            price = prices.text
+            price_schema = Price(
+                currency=currency,
+                type=type,
+                charge_type=charge_type,
+                price=Decimal(price),
+            )
+            prices_values.append(price_schema)
+    return prices_values
 
 
-async def get_direct_flights(xml_data_path: str) -> list:
-    """Get direct flight options."""
-    all_values_list = await get_all_flights(xml_data_path=xml_data_path)
-    direct_flights_list = []
+async def parse_flights(
+    target_tags: list[ET.Element], itenary_type: str
+) -> list[Flight]:
+    """
+    Extract flight information from a list of ElementTree elements.
 
-    for flight in all_values_list["Flights"]:
-        possible_direct_flight = flight["OnwardPricedItinerary"]["Flights"]["Flight"]
-        if not isinstance(possible_direct_flight, list):
-            direct_flights_list.append(flight)
+    Returns a list of Flight objects containing the extracted flight data.
+    """
+    flights_values = []
 
-    return direct_flights_list
+    for child in target_tags:
+        flight_elements = child.find(itenary_type)
+        flights = flight_elements.findall("Flights/Flight")
+        for flight in flights:
+            carrier_id = flight.find("Carrier").get("id")
+            carrier = flight.find("Carrier").text
+            flight_number = flight.find("FlightNumber").text
+            source = flight.find("Source").text
+            destination = flight.find("Destination").text
+            departure_time = flight.find("DepartureTimeStamp").text
+            arrival_time = flight.find("ArrivalTimeStamp").text
+            flight_class = flight.find("Class").text
+            number_of_stops = flight.find("NumberOfStops").text
+            fare_basis = flight.find("FareBasis").text
+            ticket_type = flight.find("TicketType").text
+            flight_schema = Flight(
+                itinerary_type=itenary_type,
+                carrier_id=carrier_id,
+                carrier=carrier,
+                flight_number=flight_number,
+                source=source,
+                destination=destination,
+                departure_time=datetime.strptime(departure_time, "%Y-%m-%dT%H%M"),
+                arrival_time=datetime.strptime(arrival_time, "%Y-%m-%dT%H%M"),
+                flight_class=flight_class,
+                number_of_stops=number_of_stops,
+                fare_basis=fare_basis,
+                ticket_type=ticket_type,
+                price_schema=itenary_type,
+            )
+            flights_values.append(flight_schema)
+
+    return flights_values
 
 
-async def get_flights_with_transfers(xml_data_path: str) -> list:
-    """Get flight options with transfers."""
-    all_values_list = await get_all_flights(xml_data_path=xml_data_path)
-    direct_flights_list = []
+async def get_all_tickets(xml_data: str) -> list[Ticket]:
+    """
+    Extracts and returns a list of ticket objects from the provided XML data.
 
-    for flight in all_values_list["Flights"]:
-        possible_direct_flight = flight["OnwardPricedItinerary"]["Flights"]["Flight"]
-        if isinstance(possible_direct_flight, list):
-            direct_flights_list.append(flight)
+    Returns a list of Ticket objects, each representing a flight ticket.
+    """
+    target_tag = await parse_all_flights(xml_data)
+    tickets = []
 
-    return direct_flights_list
+    for child in target_tag:
+        prices = await parse_pricing(target_tag)
 
+        ownard_flight_elements = child.find("OnwardPricedItinerary")
+        if ownard_flight_elements:
+            flights = await parse_flights(
+                target_tag, itenary_type="OnwardPricedItinerary"
+            )
 
-async def get_most_expensive_tickets(xml_data_path: str) -> list:
-    """Get the most expencive tickets in SingleAdult BaseFare values."""
-    all_values_list = await get_all_flights(xml_data_path=xml_data_path)
-    prices = []
-    most_expensive_tikets = []
+        return_flight_elements = child.find("ReturnPricedItinerary")
+        if return_flight_elements:
+            flights = await parse_flights(
+                target_tag, itenary_type="ReturnPricedItinerary"
+            )
 
-    for flight in all_values_list["Flights"]:
-        possible_most_expensive_tickets_price = Decimal(
-            flight["Pricing"]["ServiceCharges"][0]["text"]
+        ticket_schema = Ticket(
+            round_trip=True if return_flight_elements else False,
+            flights=flights,
+            price=prices,
         )
-        prices.append(possible_most_expensive_tickets_price)
 
-    max_price = max(prices)
+        tickets.append(ticket_schema)
 
-    for flight in all_values_list["Flights"]:
-        if Decimal(flight["Pricing"]["ServiceCharges"][0]["text"]) == max_price:
-            most_expensive_tikets.append(flight)
-
-    return most_expensive_tikets
-
-
-async def get_cheapest_tickets(xml_data_path: str) -> list:
-    """Get the cheapest tickets in SingleAdult BaseFare values."""
-    all_values_list = await get_all_flights(xml_data_path=xml_data_path)
-    prices = []
-    cheapest_tikets = []
-
-    for flight in all_values_list["Flights"]:
-        possible_cheapest_tikets_price = Decimal(
-            flight["Pricing"]["ServiceCharges"][0]["text"]
-        )
-        prices.append(possible_cheapest_tikets_price)
-
-    min_price = min(prices)
-
-    for flight in all_values_list["Flights"]:
-        if Decimal(flight["Pricing"]["ServiceCharges"][0]["text"]) == min_price:
-            cheapest_tikets.append(flight)
-
-    return cheapest_tikets
-
-
-def calculate_flight_time(flight) -> float:
-    """Calculate total flight time."""
-    flights = flight["OnwardPricedItinerary"]["Flights"]["Flight"]
-    if isinstance(flights, dict):
-        flight_start = datetime.strptime(flights["DepartureTimeStamp"], "%Y-%m-%dT%H%M")
-        flight_end = datetime.strptime(flights["ArrivalTimeStamp"], "%Y-%m-%dT%H%M")
-        possible_longest_time = flight_end - flight_start
-    if isinstance(flights, list):
-        first_flight_start = datetime.strptime(
-            flights[0]["DepartureTimeStamp"], "%Y-%m-%dT%H%M"
-        )
-        last_flight_end = datetime.strptime(
-            flights[-1]["ArrivalTimeStamp"], "%Y-%m-%dT%H%M"
-        )
-        possible_longest_time = last_flight_end - first_flight_start
-
-    return possible_longest_time.total_seconds()
-
-
-async def get_longest_flights(xml_data_path: str) -> list:
-    """Get the longest flight values."""
-    all_values_list = await get_all_flights(xml_data_path=xml_data_path)
-    times = []
-    longest_flights = []
-
-    for flight in all_values_list["Flights"]:
-        calculate_flight_time(flight)
-        times.append(calculate_flight_time(flight))
-
-    longest_time = max(times)
-    for flight in all_values_list["Flights"]:
-        if calculate_flight_time(flight) == longest_time:
-            longest_flights.append(flight)
-
-    return longest_flights
-
-
-async def get_shortest_flights(xml_data_path: str) -> list:
-    """Get the shortest flight values."""
-    all_values_list = await get_all_flights(xml_data_path=xml_data_path)
-    times = []
-    shortest_flights = []
-
-    for flight in all_values_list["Flights"]:
-        calculate_flight_time(flight)
-        times.append(calculate_flight_time(flight))
-
-    shortest_time = min(times)
-    for flight in all_values_list["Flights"]:
-        if calculate_flight_time(flight) == shortest_time:
-            shortest_flights.append(flight)
-
-    return shortest_flights
-
-
-async def most_optimal_tickets(xml_data_path: str, priority: str) -> list:
-    """Get the most optimal tickets by price or flight time."""
-    if priority == "cheapest":
-        await get_shortest_flights(xml_data_path=xml_data_path)
-    if priority == "shortest":
-        await get_cheapest_tickets(xml_data_path=xml_data_path)
+    return tickets
