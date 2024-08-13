816510639737b7ebd669456360880cf2dea027ec
Ольга Мелихова <melihova.o@yandex.ru>
2024-08-13 12:58:46 +0300

    feat: add parser for round_trip ticket types

--- app/parser_v2.py
+++ app/parser_v2.py
@@ -2,8 +2,8 @@ import xml.etree.ElementTree as ET
 
 from app.schemas import Flight, Price, Ticket
 
-xml_data = "RS_ViaOW.xml"
-
+# xml_data = "RS_ViaOW.xml"
+# xml_data = "RS_Via-3.xml"
 
 def parse_all_flights(xml_data: str) -> list[Ticket]:
     tree = ET.parse(xml_data)
@@ -14,9 +14,6 @@ def parse_all_flights(xml_data: str) -> list[Ticket]:
     target_tag = root.findall("PricedItineraries/")
     for child in target_tag:
         flight_prices = child.find("Pricing")
-        ownard_flight_elements = child.find("OnwardPricedItinerary")
-        return_flight_elements = child.find("ReturnPricedItinerary")
-
         currency = flight_prices.get("currency")
         service_charges = flight_prices.findall("ServiceCharges")
 
@@ -34,8 +31,10 @@ def parse_all_flights(xml_data: str) -> list[Ticket]:
             prices_values.append(price_schema)
 
         flights_values = []
-        flights = ownard_flight_elements.findall("Flights/Flight")
-        for flight in flights:
+
+        ownard_flight_elements = child.find("OnwardPricedItinerary")
+        ownard_flights = ownard_flight_elements.findall("Flights/Flight")
+        for flight in ownard_flights:
             carrier_id = flight.find("Carrier").get("id")
             carrier = flight.find("Carrier").text
             flight_number = flight.find("FlightNumber").text
@@ -64,15 +63,48 @@ def parse_all_flights(xml_data: str) -> list[Ticket]:
             )
             flights_values.append(flight_schema)
 
+        return_flight_elements = child.find("ReturnPricedItinerary")        
+        if return_flight_elements:
+            return_flights = return_flight_elements.findall("Flights/Flight")
+            for flight in return_flights:
+                carrier_id = flight.find("Carrier").get("id")
+                carrier = flight.find("Carrier").text
+                flight_number = flight.find("FlightNumber").text
+                source = flight.find("Source").text
+                destination = flight.find("Destination").text
+                departure_time = flight.find("DepartureTimeStamp").text
+                arrival_time = flight.find("ArrivalTimeStamp").text
+                flight_class = flight.find("Class").text
+                number_of_stops = flight.find("NumberOfStops").text
+                fare_basis = flight.find("FareBasis").text
+                ticket_type = flight.find("TicketType").text
+                flight_schema = Flight(
+                    itinerary_type="return",
+                    carrier_id=carrier_id,
+                    carrier=carrier,
+                    flight_number=flight_number,
+                    source=source,
+                    destination=destination,
+                    departure_time=departure_time,
+                    arrival_time=arrival_time,
+                    flight_class=flight_class,
+                    number_of_stops=number_of_stops,
+                    fare_basis=fare_basis,
+                    ticket_type=ticket_type,
+                    price_schema=price_schema,
+                )
+                flights_values.append(flight_schema)
+
         ticket_schema = Ticket(
-            round_trip=False,
+            round_trip=True if return_flight_elements else False,
             flights=flights_values,
             price=prices_values,
         )
 
         tickets.append(ticket_schema)
 
+    # print(tickets)
     return tickets
 
 
-parse_all_flights(xml_data)
+# parse_all_flights(xml_data)
